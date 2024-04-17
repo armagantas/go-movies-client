9758b2f906dc9fac9795738bf276e74acd6be74b
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-04-18 02:09:05 +0300

    components and edit movie page

--- src/components/EditMovie.jsx
+++ src/components/EditMovie.jsx
@@ -1,10 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, useOutletContext, useParams } from "react-router-dom";
+import Input from "./Form/Input";
 
 const EditMovie = () => {
+  const navigate = useNavigate();
+  const { jwtToken } = useOutletContext();
+
+  const [error, setError] = useState(null);
+  const [errors, setErrors] = useState([]);
+
+  const hasError = (key) => {
+    return errors.indexOf(key) !== -1;
+  };
+
+  const [movie, setMovie] = useState({
+    id: 0,
+    title: "",
+    release_date: "",
+    runtime: "",
+    mpaa_rating: "",
+    description: "",
+  });
+
+  //get id from url
+  let { id } = useParams();
+
+  useEffect(() => {
+    if (jwtToken === "") {
+      navigate("/login");
+      return;
+    }
+  }, [jwtToken, navigate]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const handleChange = () => (event) => {
+    let value = event.target.value;
+    let name = event.target.name;
+    setMovie({
+      ...movie,
+      [name]: value,
+    });
+  };
+
   return (
     <div>
-      <h2>EditMovie</h2>
+      <h2>Add/Edit Movie</h2>
       <hr />
+      <pre>{JSON.stringify(movie, null, 3)}</pre>
+      <form onSubmit={handleSubmit}>
+        <input type="hidden" name="id" value={movie.id} id="id">
+          <Input
+            title={"Title"}
+            className={"form-control"}
+            type={"text"}
+            name={"title"}
+            value={movie.title}
+            onChange={handleChange("title")}
+            errorDiv={hasError("title") ? "text-danger" : "d-non"}
+            errorMsg={"Please enter a title"}
+          />
+        </input>
+      </form>
     </div>
   );
 };
---
+++ src/components/Form/Checkbox.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Checkbox = (props) => {
+  return (
+    <div className="form-check">
+      <input
+        id={props.name}
+        className="form-check-input"
+        type="checkbox"
+        value={props.value}
+        name={props.name}
+        onChange={props.onChange}
+        checked={props.checked}
+      />
+      <label htmlFor={props.name} className="form-check-label">
+        {props.title}
+      </label>
+    </div>
+  );
+};
+
+export default Checkbox;
